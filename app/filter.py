178f5b0cc381e82a190b003555afeac60b474ef1
jyu748 <jyu748@aucklanduni.ac.nz>
2022-11-23 11:07:52 +1300

    update filter_comp_info

--- app/filter.py
+++ app/filter.py
@@ -1,25 +1,64 @@
 import re
 
-MAPPED_MIME_TYPES = {
-    # "CSV": ["text/csv"],
-    # "SEGMENTATION_FILES": ["application/vnd.mbfbioscience.metadata+xml", "application/vnd.mbfbioscience.neurolucida+xml"],
-    # "CONTEXT_FILE": ["application/x.vnd.abi.context-information+json"],
-    "Scaffold": ["application/x.vnd.abi.scaffold.meta+json", "inode/vnd.abi.scaffold+file"],
-    # "SCAFFOLD_VIEW_FILE": ["application/x.vnd.abi.scaffold.view+json", "inode/vnd.abi.scaffold.view+file"],
-    # "SIMULATION_FILE": ["application/x.vnd.abi.simulation+json"],
-    # "THUMBNAIL_IMAGE": ["image/x.vnd.abi.thumbnail+jpeg", "inode/vnd.abi.scaffold+thumbnail", "inode/vnd.abi.scaffold.thumbnail+file"],
-    # "SCAFFOLD_DIR": ["inode/vnd.abi.scaffold+directory"],
-    "Plot": ["text/vnd.abi.plot+tab-separated-values", "text/vnd.abi.plot+csv"],
-    # "COMMON_IMAGES": ["image/png", "image/jpeg"],
-    # "tiff-image": ["image/tiff", "image/tif"],
-    # "BIOLUCIDA_3D": ["image/jpx", "image/vnd.ome.xml+jpx"],
-    # "BIOLUCIDA_2D": ["image/jp2", "image/vnd.ome.xml+jp2"],
-    # "VIDEO": ["video/mp4"]
-}
-
-MAPPED_FUNDING = {
-    "Funding A": ["OT3OD025349"],
-    "Funding B": ["OT2OD023848"]
+FILTERS = {
+    "MAPPED_MIME_TYPES": {
+        "title": "MIME TYPES",
+        "node": "manifest",
+        "field": "additional_types",
+        "element": {
+            # "CSV": ["text/csv"],
+            # "SEGMENTATION_FILES": ["application/vnd.mbfbioscience.metadata+xml", "application/vnd.mbfbioscience.neurolucida+xml"],
+            # "CONTEXT_FILE": ["application/x.vnd.abi.context-information+json"],
+            "Scaffold": ["application/x.vnd.abi.scaffold.meta+json", "inode/vnd.abi.scaffold+file"],
+            # "SCAFFOLD_VIEW_FILE": ["application/x.vnd.abi.scaffold.view+json", "inode/vnd.abi.scaffold.view+file"],
+            # "SIMULATION_FILE": ["application/x.vnd.abi.simulation+json"],
+            # "THUMBNAIL_IMAGE": ["image/x.vnd.abi.thumbnail+jpeg", "inode/vnd.abi.scaffold+thumbnail", "inode/vnd.abi.scaffold.thumbnail+file"],
+            # "SCAFFOLD_DIR": ["inode/vnd.abi.scaffold+directory"],
+            "Plot": ["text/vnd.abi.plot+tab-separated-values", "text/vnd.abi.plot+csv"],
+            # "COMMON_IMAGES": ["image/png", "image/jpeg"],
+            # "tiff-image": ["image/tiff", "image/tif"],
+            # "BIOLUCIDA_3D": ["image/jpx", "image/vnd.ome.xml+jpx"],
+            # "BIOLUCIDA_2D": ["image/jp2", "image/vnd.ome.xml+jp2"],
+            # "VIDEO": ["video/mp4"]
+        }
+    },
+    "MAPPED_FUNDING": {
+        "title": "FUNDINGS",
+        "node": "dataset_description",
+        "field": "funding",
+        "element": {
+            "Funding A": ["OT3OD025349"],
+            "Funding B": ["OT2OD023848"]}
+    },
+    "TEST 1": {
+        "title": "TEST TYPES",
+        "node": "manifest",
+        "field": "additional_types",
+        "element": {
+            # "CSV": ["text/csv"],
+            # "SEGMENTATION_FILES": ["application/vnd.mbfbioscience.metadata+xml", "application/vnd.mbfbioscience.neurolucida+xml"],
+            # "CONTEXT_FILE": ["application/x.vnd.abi.context-information+json"],
+            "Scaffold": ["application/x.vnd.abi.scaffold.meta+json", "inode/vnd.abi.scaffold+file"],
+            # "SCAFFOLD_VIEW_FILE": ["application/x.vnd.abi.scaffold.view+json", "inode/vnd.abi.scaffold.view+file"],
+            # "SIMULATION_FILE": ["application/x.vnd.abi.simulation+json"],
+            # "THUMBNAIL_IMAGE": ["image/x.vnd.abi.thumbnail+jpeg", "inode/vnd.abi.scaffold+thumbnail", "inode/vnd.abi.scaffold.thumbnail+file"],
+            # "SCAFFOLD_DIR": ["inode/vnd.abi.scaffold+directory"],
+            "Plot": ["text/vnd.abi.plot+tab-separated-values", "text/vnd.abi.plot+csv"],
+            # "COMMON_IMAGES": ["image/png", "image/jpeg"],
+            # "tiff-image": ["image/tiff", "image/tif"],
+            # "BIOLUCIDA_3D": ["image/jpx", "image/vnd.ome.xml+jpx"],
+            # "BIOLUCIDA_2D": ["image/jp2", "image/vnd.ome.xml+jp2"],
+            # "VIDEO": ["video/mp4"]
+        }
+    },
+    "TEST 2": {
+        "title": "TESTS",
+        "node": "dataset_description",
+        "field": "funding",
+        "element": {
+            "Funding A": ["OT3OD025349"],
+            "Funding B": ["OT2OD023848"]}
+    }
 }
 
 
@@ -61,12 +100,16 @@ class Filter:
         return list(set(dataset_list))
 
     def generate_filter_data(self):
-        filter_component_info = {
-            "title": ["DATA TYPES", "FUNDING", "TEST TITLE 1", "TEST TITLE 2"],
-            "node": ["manifest", "dataset_description", "test node 1", "test node 2"],
-            "field": ["additional_types", "funding", "test 1", "test 2"],
-            "element": {
-                "additional_types": MAPPED_MIME_TYPES, "funding": MAPPED_FUNDING, "test 1": MAPPED_MIME_TYPES, "test 2": MAPPED_FUNDING
-            }
+        filter_comp_info = {
+            "size": len(FILTERS),
+            "titles": [],
+            "nodes": [],
+            "fields": [],
+            "elements": []
         }
-        return filter_component_info
+        for ele in FILTERS:
+            filter_comp_info["titles"].append(FILTERS[ele]["title"])
+            filter_comp_info["nodes"].append(FILTERS[ele]["node"])
+            filter_comp_info["fields"].append(FILTERS[ele]["field"])
+            filter_comp_info["elements"].append(FILTERS[ele]["element"])
+        return filter_comp_info
