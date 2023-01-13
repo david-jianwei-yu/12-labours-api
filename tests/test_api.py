e13ed1be298d88ec2392a08360de5c6a7ad30588
jyu748 <jyu748@aucklanduni.ac.nz>
2023-01-13 09:45:39 +0800

    update api testing

--- tests/test_api.py
+++ tests/test_api.py
@@ -276,24 +276,24 @@ def test_get_irods_collections(client):
 
 def test_get_irods_data_file(client):
     ACTION = "preview"
-    FILEPATH = "datasets/dataset-217-version-2/derivative/scaffold_context_info.json"
+    FILEPATH = "dataset-217-version-2/derivative/scaffold_context_info.json"
     response = client.get(f"/data/{ACTION}/{FILEPATH}")
     result = response.json()
     assert response.status_code == 200
-    assert result == {"description": "Annotated brainstem scaffold for pig available for registration of segmented neural anatomical-functional mapping of neural circuits.",
-                      "heading": "Generic pig brainstem scaffold", "id": "sparc.science.context_data", "samples": [], "version": "0.1.0", "views": []}
+    assert result["description"] == "Annotated brainstem scaffold for pig available for registration of segmented neural anatomical-functional mapping of neural circuits."
+    assert result["heading"] == "Generic pig brainstem scaffold"
 
     ACTION = "preview"
-    INVALID_FILEPATH = "datasets/dataset-217-version-2/derivative/scaffold_context_info"
+    INVALID_FILEPATH = "dataset-217-version-2/derivative/scaffold_context_info"
     response = client.get(f"/data/{ACTION}/{INVALID_FILEPATH}")
     result = response.json()
     assert response.status_code == 404
     assert result["detail"] == "Data not found in the provided path"
 
     INVALID_ACTION = "preload"
-    FILEPATH = "datasets/dataset-217-version-2/derivative/scaffold_context_info.json"
+    FILEPATH = "dataset-217-version-2/derivative/scaffold_context_info.json"
     response = client.get(f"/data/{INVALID_ACTION}/{FILEPATH}")
+    result = response.json()
     assert response.status_code == 422
     # assert response.status_code == 405
-    # assert response.json()[
-    #     "detail"] == "The action is not provided in this API"
+    # assert result["detail"] == "The action is not provided in this API"
