e00f4489520a16578a5752df5a72220fdf5bb179
jyu748 <jyu748@aucklanduni.ac.nz>
2023-03-16 11:52:32 +1300

    remove hard code url

--- app/pagination.py
+++ app/pagination.py
@@ -113,7 +113,7 @@ class Pagination:
     def handle_image_url(self, filetype, id, filename, source_of):
         url_suffix = ""
         if filetype == "scaffoldViews" or filetype == "thumbnails":
-            url_suffix = f"http://localhost:8000/data/preview/{id}"
+            url_suffix = f"/data/preview/{id}"
             if source_of != "":
                 path_list = filename.split("/")
                 path_list[-1] = source_of.split("/")[-1]
@@ -161,7 +161,7 @@ class Pagination:
                 "numberSamples": int(ele["dataset_descriptions"][0]["number_of_samples"][0]),
                 "numberSubjects": int(ele["dataset_descriptions"][0]["number_of_subjects"][0]),
                 "name": ele["dataset_descriptions"][0]["title"][0],
-                "url": "https://staging.12-labours-demo.org/data/browser/dataset/" + ele["submitter_id"] + "?datasetTab=abstract",
+                "url": "",
                 "datasetId": ele["submitter_id"],
                 "organs": ele["dataset_descriptions"][0]["study_organ_system"],
                 "species": self.update_species(ele["cases"]),
