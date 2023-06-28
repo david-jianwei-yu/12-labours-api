36d0c6f91ca552242cba16fc349bcbe4c458ce08
ddjnw1yu <david.jianwei.yu@outlook.com>
2023-06-28 16:23:30 +1200

    create user class
    provide multiple user access control functionality

--- middleware/auth.py
+++ middleware/auth.py
@@ -12,18 +12,50 @@ from app.config import iRODSConfig
 security = HTTPBearer()
 
 
+class User:
+    def __init__(self, email, policies):
+        self.user_email = email
+        self.user_policies = policies
+
+    def get_user_detail(self):
+        user = {
+            "email": self.user_email,
+            "policies": self.user_policies
+        }
+        return user
+
+
 class Authenticator:
     def __init__(self):
         self.key = Fernet.generate_key()
         self.fernet = Fernet(self.key)
-        self.access_token = None
-        self.authorized_email = None
         self.public_access = False
+        self.authorized_email = []
+        self.authorized_user = []
         self.current_user = None
 
-    def match_user_policies(self, email, userinfo):
+    def create_user_authority(self, email, userinfo):
         if email in userinfo:
-            return userinfo[email]["policies"]
+            self.public_access = False
+            if email not in self.authorized_email:
+                self.authorized_email.append(email)
+                user = User(email, userinfo[email]["policies"])
+                self.authorized_user.append(user)
+                return user
+            else:
+                raise HTTPException(
+                    status_code=status.HTTP_409_CONFLICT, detail=f"Email {email} has already been authorized")
+        else:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND, detail=f"Email {email} is not authorized")
+
+    def revoke_user_authority(self, email):
+        if email in self.authorized_email:
+            index = self.authorized_email.index(email)
+            self.authorized_email.pop(index)
+            self.authorized_user.pop(index)
+            self.public_access = True
+            return self.public_access
         else:
             raise HTTPException(
                 status_code=status.HTTP_404_NOT_FOUND, detail=f"Email {email} is not authorized")
@@ -37,15 +69,11 @@ class Authenticator:
                 yaml_string += str(line, encoding='utf-8')
         yaml_dict = yaml.load(yaml_string, Loader=SafeLoader)
         yaml_json = json.loads(json.dumps(yaml_dict))["users"]
-        policy_list = self.match_user_policies(email, yaml_json)
-        self.authorized_email = email
-        self.public_access = False
-        gen3_access = {
-            "email": email,
-            "policies": policy_list
-        }
-        self.access_token = self.fernet.encrypt(str(gen3_access).encode())
-        return self.access_token
+
+        user = self.create_user_authority(email, yaml_json)
+        access_token = self.fernet.encrypt(
+            str(user.get_user_detail()).encode())
+        return access_token
 
     def authenticate_token(self, token):
         try:
@@ -53,13 +81,16 @@ class Authenticator:
                 self.public_access = True
                 return self.public_access
             else:
-                self.current_user = json.loads(
-                    re.sub("'", '"', self.fernet.decrypt(token).decode()))
-                return self.current_user["email"] == self.authorized_email
+                email = json.loads(
+                    re.sub("'", '"', self.fernet.decrypt(token).decode()))["email"]
+                if email in self.authorized_email:
+                    index = self.authorized_email.index(email)
+                    self.current_user = self.authorized_user[index]
+                    return True
         except Exception:
             pass
 
-    async def get_user_authority(self,  token: HTTPAuthorizationCredentials = Depends(security)):
+    async def get_user_access_scope(self,  token: HTTPAuthorizationCredentials = Depends(security)):
         if not self.authenticate_token(token.credentials):
             raise HTTPException(
                 status_code=status.HTTP_401_UNAUTHORIZED,
@@ -71,5 +102,6 @@ class Authenticator:
             "policies": ["demo1"],
         }
         if not self.public_access:
-            result["policies"] = self.current_user["policies"]
+            result["policies"] = self.current_user.get_user_detail()[
+                "policies"]
         return result
