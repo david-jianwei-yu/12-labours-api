1380841bd18d2b8c79fdf25f0f9cfa23ac7333cf
ddjnw1yu <david.jianwei.yu@outlook.com>
2023-08-23 10:49:51 +1200

    fix scaffold/plot match issue

--- app/query.py
+++ app/query.py
@@ -1,35 +1,42 @@
 class Query(object):
     def __init__(self, fg):
         self.FILTERS = fg.get_filters()
-        self.exist = []
+        self.added = []
         self.facet_dict = []
 
-    def add_matched_facet(self, field, data_type):
-        mapped_element = f"MAPPED_{field.upper()}"
-        for key, value in self.FILTERS[mapped_element]["facets"].items():
+    def add_matched_facet(self, field_key, field_value):
+        mapped_element = f"MAPPED_{field_key.upper()}"
+        for facet_key, facet_value in self.FILTERS[mapped_element]["facets"].items():
             facet_obj = {}
-            if value == data_type[field] and key not in self.exist:
-                facet_obj["facet"] = key
+            is_match = False
+            if type(facet_value) == str and field_value == facet_value:
+                is_match = True
+            elif type(facet_value) == list and field_value in facet_value:
+                is_match = True
+            if is_match and facet_key not in self.added:
+                facet_obj["facet"] = facet_key
                 facet_obj["term"] = self.FILTERS[mapped_element]["title"]
                 facet_obj["facetPropPath"] = self.FILTERS[mapped_element]["node"] + \
                     ">" + self.FILTERS[mapped_element]["field"]
-                self.exist.append(key)
+                self.added.append(facet_key)
                 self.facet_dict.append(facet_obj)
 
     def generate_related_facet(self, data):
         if "dataset_descriptions" in data.keys() and data["dataset_descriptions"] != []:
             for dataset_description in data["dataset_descriptions"]:
                 self.add_matched_facet(
-                    "study_organ_system", dataset_description)
+                    "study_organ_system", dataset_description["study_organ_system"])
         if "scaffolds" in data.keys() and data["scaffolds"] != []:
             for scaffold in data["scaffolds"]:
-                self.add_matched_facet("additional_types", scaffold)
+                self.add_matched_facet(
+                    "additional_types", scaffold["additional_types"])
         if "plots" in data.keys() and data["plots"] != []:
             for plot in data["plots"]:
-                self.add_matched_facet("additional_types", plot)
+                self.add_matched_facet(
+                    "additional_types", plot["additional_types"])
         if "cases" in data.keys() and data["cases"] != []:
             for case in data["cases"]:
-                self.add_matched_facet("age_category", case)
-                self.add_matched_facet("sex", case)
-                self.add_matched_facet("species", case)
+                self.add_matched_facet("age_category", case["age_category"])
+                self.add_matched_facet("sex", case["sex"])
+                self.add_matched_facet("species", case["species"])
         return self.facet_dict
