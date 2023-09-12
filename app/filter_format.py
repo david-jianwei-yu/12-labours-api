c15b5eba4b72803ac675375947464e3bd18dcee8
ddjnw1yu <david.jianwei.yu@outlook.com>
2023-09-13 11:51:02 +1200

    update naming and function

--- app/filter_format.py
+++ app/filter_format.py
@@ -11,59 +11,56 @@ class FilterFormat:
     """
 
     def __init__(self, fg):
-        self.FG = fg
-        self.MAPPED_FILTERS = fg.get_mapped_filter()
+        self._fg = fg
+        self._mapped_filter = fg.get_mapped_filter()
+
+    def _handle_element_content(self, mapped_element, access_scope):
+        """
+        Handler for switching element content between public and private
+        """
+        private_filter = self._fg.generate_private_filter(access_scope)
+        if mapped_element in private_filter:
+            return private_filter[mapped_element]
+        return self._mapped_filter[mapped_element]
 
     def generate_sidebar_filter_format(self, access_scope):
         """
-        Format for map viewer sidebar
+        Format for portal map integrated viewer sidebar
         """
-        sidebar_filter_format = []
-        for mapped_element in self.MAPPED_FILTERS:
-            used_filter = self.FG.set_filter(mapped_element, access_scope)
-            filter_parent = {
+        sidebar_format = []
+        for mapped_element in self._mapped_filter:
+            content = self._handle_element_content(mapped_element, access_scope)
+            parent_format = {
                 "key": "",
                 "label": "",
                 "children": [],
             }
-            filter_parent["key"] = (
-                used_filter[mapped_element]["node"]
-                + ">"
-                + used_filter[mapped_element]["field"]
-            )
-            filter_parent["label"] = used_filter[mapped_element]["title"].capitalize()
-            for facet_name in used_filter[mapped_element]["facets"]:
-                filter_children = {
+            parent_format["key"] = f"{content['node']}>{content['field']}"
+            parent_format["label"] = content["title"].capitalize()
+            for facet_name in content["facets"]:
+                children_format = {
                     "facetPropPath": "",
                     "label": "",
                 }
-                filter_children["facetPropPath"] = filter_parent["key"]
-                filter_children["label"] = facet_name
-                filter_parent["children"].append(filter_children)
-            sidebar_filter_format.append(filter_parent)
-        return sidebar_filter_format
+                children_format["facetPropPath"] = parent_format["key"]
+                children_format["label"] = facet_name
+                parent_format["children"].append(children_format)
+            sidebar_format.append(parent_format)
+        return sidebar_format
 
     def generate_filter_format(self, access_scope):
         """
-        Format for data browser
+        Format for portal data browser
         """
-        filter_format = {
-            "size": len(self.MAPPED_FILTERS),
+        format_ = {
+            "size": len(self._mapped_filter),
             "titles": [],
             "nodes>fields": [],
             "elements": [],
         }
-        for mapped_element in self.MAPPED_FILTERS:
-            used_filter = self.FG.set_filter(mapped_element, access_scope)
-            filter_format["titles"].append(
-                used_filter[mapped_element]["title"].capitalize()
-            )
-            filter_format["nodes>fields"].append(
-                used_filter[mapped_element]["node"]
-                + ">"
-                + used_filter[mapped_element]["field"]
-            )
-            filter_format["elements"].append(
-                list(used_filter[mapped_element]["facets"].keys())
-            )
-        return filter_format
+        for mapped_element in self._mapped_filter:
+            content = self._handle_element_content(mapped_element, access_scope)
+            format_["titles"].append(content["title"].capitalize())
+            format_["nodes>fields"].append(f"{content['node']}>{content['field']}")
+            format_["elements"].append(list(content["facets"].keys()))
+        return format_
