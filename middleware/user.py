4887c9d7000ee58e1c91511167831c7a576b910e
ddjnw1yu <david.jianwei.yu@outlook.com>
2023-09-14 01:26:08 +1200

    apply code standards to user.py

--- middleware/user.py
+++ middleware/user.py
@@ -1,22 +1,46 @@
+"""
+Functionality for creating gen3 access users to support portal access control
+- get_user_identity
+- get_user_access_scope
+- get_user_expire_time
+- get_user_detail
+"""
+
+
 class User:
-    def __init__(self, identity, scope, expire_time):
+    """
+    identity, access_scope, expire_time are required to create user
+    """
+
+    def __init__(self, identity, access_scope, expire_time):
         self.identity = identity
-        self.scope = scope
+        self.access_scope = access_scope
         self.expire_time = expire_time
 
     def get_user_identity(self):
+        """
+        Handler for returning user identity
+        """
         return self.identity
 
-    def get_user_scope(self):
-        return self.scope
+    def get_user_access_scope(self):
+        """
+        Handler for returning user access scope
+        """
+        return self.access_scope
 
     def get_user_expire_time(self):
+        """
+        Handler for returning user expire time
+        """
         return self.expire_time
 
     def get_user_detail(self):
-        user = {
+        """
+        Handler for returning user detail
+        """
+        return {
             "identity": self.identity,
-            "scope": self.scope,
+            "access_scope": self.access_scope,
             "expire_time": self.expire_time,
         }
-        return user
