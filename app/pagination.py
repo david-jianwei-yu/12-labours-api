8b2e0acd97f9e61d0c765e9563680a2f6e839e4b
ddjnw1yu <david.jianwei.yu@outlook.com>
2023-09-14 01:08:59 +1200

    apply code standards to pagination.py

--- app/pagination.py
+++ app/pagination.py
@@ -1,3 +1,9 @@
+"""
+Functionality for processing pagination related logic
+- gain_pagination_data
+- gain_pagination_count
+- process_pagination_item
+"""
 import copy
 import json
 import queue
@@ -10,15 +16,25 @@ from app.data_schema import GraphQLPaginationItem, GraphQLQueryItem
 
 
 class Pagination:
+    """
+    fg -> filter generator object is required
+    f -> filter object is required
+    s -> search object is required
+    sgqlc -> simple graphql client object is required
+    """
+
     def __init__(self, fg, f, s, sgqlc):
-        self.FG = fg
-        self.MAPPED_FILTERS = fg.get_mapped_filter()
-        self.F = f
-        self.S = s
-        self.SGQLC = sgqlc
+        self._fg = fg
+        self._mapped_filter = fg.get_mapped_filter()
+        self._f = f
+        self._s = s
+        self._sgqlc = sgqlc
         self.public_access = [Gen3Config.GEN3_PUBLIC_ACCESS]
 
-    def generate_dictionary(self, data):
+    def _handle_dataset(self, data):
+        """
+        Handler for generating dataset dictionary
+        """
         dataset_dict = {}
         for ele in data:
             dataset_id = ele["submitter_id"]
@@ -26,39 +42,46 @@ class Pagination:
                 dataset_dict[dataset_id] = ele
         return dataset_dict
 
-    def threading_fetch(self, items):
-        result_queue = queue.Queue()
-        threads = []
+    def _handle_thread_fetch(self, items):
+        """
+        Handler for using thread to fetch data
+        """
+        queue_ = queue.Queue()
+        threads_pool = []
         for args in items:
-            t = threading.Thread(
-                target=self.SGQLC.get_queried_result, args=(*args, result_queue)
+            thread = threading.Thread(
+                target=self._sgqlc.fetch_queried_result, args=(*args, queue_)
             )
-            threads.append(t)
-            t.start()
-        for t in threads:
-            t.join()
-
+            threads_pool.append(thread)
+            thread.start()
+        for thread in threads_pool:
+            thread.join()
         result = {}
-        while not result_queue.empty():
-            data = result_queue.get()
+        while not queue_.empty():
+            data = queue_.get()
             result.update(data)
         return result
 
-    def handle_order_by_dataset_description(self, filter):
+    def _handle_order_by_dataset_description(self, filter_):
+        """
+        Handler for updating submitter_id for order by dataset description
+        """
         result = {}
-        if "submitter_id" in filter:
+        if "submitter_id" in filter_:
             result["submitter_id"] = []
-            for dataset in filter["submitter_id"]:
+            for dataset in filter_["submitter_id"]:
                 result["submitter_id"].append(f"{dataset}-dataset_description")
         return result
 
-    def get_pagination_order(self, item):
-        filter_dict = self.handle_order_by_dataset_description(item.filter)
+    def _handle_pagination_order(self, item):
+        """
+        Handler for updating pagination data order
+        """
         query_item = GraphQLQueryItem(
             node="pagination_order_by_dataset_description",
             limit=item.limit,
             page=item.page,
-            filter=filter_dict,
+            filter=self._handle_order_by_dataset_description(item.filter),
             access=item.access,
             asc=item.asc,
             desc=item.desc,
@@ -67,22 +90,22 @@ class Pagination:
             query_item.asc = "title"
         elif "desc" in item.order:
             query_item.desc = "title"
-        query_result = self.SGQLC.get_queried_result(query_item)
-
         # Include both public and private if have the access
-        ordered_dataset = []
-        for ele in query_result:
+        ordered_datasets = []
+        for ele in self._sgqlc.fetch_queried_result(query_item):
             dataset_id = ele["experiments"][0]["submitter_id"]
-            if dataset_id not in ordered_dataset:
-                ordered_dataset.append(dataset_id)
-        return ordered_dataset
+            if dataset_id not in ordered_datasets:
+                ordered_datasets.append(dataset_id)
+        return ordered_datasets
 
-    def get_pagination_data(self, item, match_pair, is_public_access_filtered):
+    def gain_pagination_data(self, item, match_pair, is_public_access_filtered):
+        """
+        Handler for fetching data based on pagination item
+        """
         if "title" in item.order.lower():
             # Get an ordered filter
-            item.filter["submitter_id"] = self.get_pagination_order(item)
+            item.filter["submitter_id"] = self._handle_pagination_order(item)
             item.page = 1
-
         query_item = GraphQLPaginationItem(
             limit=item.limit,
             page=item.page,
@@ -92,9 +115,9 @@ class Pagination:
             asc=item.asc,
             desc=item.desc,
         )
-        query_result = self.SGQLC.get_queried_result(query_item)
-        displayed_dataset = self.generate_dictionary(query_result)
-
+        displayed_dataset = self._handle_dataset(
+            self._sgqlc.fetch_queried_result(query_item)
+        )
         item.access.remove(self.public_access[0])
         items = []
         # Query displayed datasets which have private version
@@ -107,16 +130,16 @@ class Pagination:
                         access=item.access,
                     )
                     items.append((query_item, dataset))
-
-        private_replacement = self.threading_fetch(items)
-
         if not is_public_access_filtered:
             # Replace the dataset if it has a private version
-            for dataset in private_replacement.keys():
-                displayed_dataset[dataset] = private_replacement[dataset][0]
+            for dataset, data in self._handle_thread_fetch(items).items():
+                displayed_dataset[dataset] = data[0]
         return list(displayed_dataset.values())
 
-    def get_pagination_count(self, item):
+    def gain_pagination_count(self, item):
+        """
+        Handler for processing the number of data based on pagination item
+        """
         private_access = copy.deepcopy(item.access)
         private_access.remove(self.public_access[0])
         user_access = {
@@ -131,74 +154,82 @@ class Pagination:
                 node="experiment_pagination_count", filter=item.filter, access=value
             )
             items.append((pagination_count_item, key))
-
-        fetched_data = self.threading_fetch(items)
-
-        public_result = self.generate_dictionary(fetched_data["public_access"])
-        private_result = self.generate_dictionary(fetched_data["private_access"])
+        fetched_data = self._handle_thread_fetch(items)
+        public_result = self._handle_dataset(fetched_data["public_access"])
+        private_result = self._handle_dataset(fetched_data["private_access"])
         # Default datasets exist in public repository only,
         # Will contain all available datasets after updating
         displayed_dataset = list(public_result.keys())
         # Datasets which exist in both public and private repository will be added to match_pair
         # It will be used to help achieve priority presentation of private datasets
         match_pair = []
-        for id in private_result.keys():
-            if id not in public_result:
-                displayed_dataset.append(id)
+        for dataset_id in private_result:
+            if dataset_id not in public_result:
+                displayed_dataset.append(dataset_id)
             else:
-                match_pair.append(id)
+                match_pair.append(dataset_id)
         return len(displayed_dataset), match_pair
 
-    def handle_pagination_item_filter(self, filter_field, facet_name, private_filter):
+    def _handle_pagination_item_filter(self, filter_field, facets, private_filter):
+        """
+        Handler for updating filter in pagination item
+        """
         value_list = []
-        for facet in facet_name:
+        for facet in facets:
             # Use .capitalize() to make it non-case sensitive
             # Avoid mis-match
             facet_name = facet.capitalize()
-            for mapped_element in self.MAPPED_FILTERS:
+            for mapped_element in self._mapped_filter:
                 if mapped_element in private_filter:
-                    filter_dict = private_filter
+                    content = private_filter[mapped_element]
                 else:
-                    filter_dict = self.MAPPED_FILTERS
+                    content = self._mapped_filter[mapped_element]
                 # Check if title can match with a exist filter object
-                if filter_dict[mapped_element]["field"] == filter_field:
+                if content["field"] == filter_field:
                     # Check if ele_name is a key under filter object element field
-                    if facet_name not in filter_dict[mapped_element]["facets"]:
+                    if facet_name not in content["facets"]:
                         raise HTTPException(
                             status_code=status.HTTP_400_BAD_REQUEST,
                             detail="Invalid or unauthorized facet passed in",
                         )
 
-                    facet_value = filter_dict[mapped_element]["facets"][facet_name]
-                    if type(facet_value) == list:
+                    facet_value = content["facets"][facet_name]
+                    if isinstance(facet_value, list):
                         value_list.extend(facet_value)
                     else:
                         value_list.append(facet_value)
         return {filter_field: value_list}
 
-    def handle_access(self, access_scope):
+    def _handle_access(self, access_scope):
+        """
+        Handler for generating private access
+        """
         private_access = []
         for scope in access_scope:
             if scope != self.public_access[0]:
                 private_access.append(scope)
         return private_access
 
-    def update_pagination_item(self, item, input):
+    def process_pagination_item(self, item, input_):
+        """
+        Handler for process pagination item to fit the query code generator format
+        """
         is_public_access_filtered = False
         has_search_result = False
 
         # FILTER
         if item.filter != {}:
-            private_access = self.handle_access(item.access)
-            private_filter = self.FG.generate_private_filter(private_access)
+            private_filter = self._fg.generate_private_filter(
+                self._handle_access(item.access)
+            )
             items = []
             filter_dict = {"submitter_id": []}
-            for node_filed, facet_name in item.filter.items():
+            for node_filed, facets in item.filter.items():
                 filter_node = node_filed.split(">")[0]
                 filter_field = node_filed.split(">")[1]
                 # Update filter based on authority
-                valid_filter = self.handle_pagination_item_filter(
-                    filter_field, facet_name, private_filter
+                valid_filter = self._handle_pagination_item_filter(
+                    filter_field, facets, private_filter
                 )
                 query_item = GraphQLQueryItem(
                     node=filter_node, filter=valid_filter, access=self.public_access
@@ -209,29 +240,23 @@ class Pagination:
                         is_public_access_filtered = True
                 else:
                     query_item.access = item.access
-                key = json.dumps(valid_filter)
-                items.append((query_item, key))
-
-            fetched_data = self.threading_fetch(items)
-
-            for key in fetched_data:
-                filter = json.loads(key)
-                filtered_dataset = self.F.get_filtered_dataset(
-                    filter, fetched_data[key]
+                items.append((query_item, json.dumps(valid_filter)))
+            for filter_, related_data in self._handle_thread_fetch(items).items():
+                filter_dict["submitter_id"].append(
+                    self._f.generate_filtered_dataset(json.loads(filter_), related_data)
                 )
-                filter_dict["submitter_id"].append(filtered_dataset)
             item.filter = filter_dict
-            self.F.filter_relation(item)
+            self._f.implement_filter_relation(item)
 
         # SEARCH
-        if input != "":
-            # If input does not match any content in the database, item.search will be empty dictionary
-            item.search["submitter_id"] = self.S.get_searched_dataset(input)
+        if input_ != "":
+            # If input does not match any content in the database, item.search will be empty
+            item.search["submitter_id"] = self._s.generate_searched_dataset(input_)
             if item.search != {} and (
                 "submitter_id" not in item.filter or item.filter["submitter_id"] != []
             ):
                 has_search_result = True
-                self.S.search_filter_relation(item)
+                self._s.implement_search_filter_relation(item)
 
         # ORDER
         order_type = item.order.lower()
