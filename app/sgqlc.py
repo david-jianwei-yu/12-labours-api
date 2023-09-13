54c9e39f7187fe2078a0bca9463376234ca1168c
ddjnw1yu <david.jianwei.yu@outlook.com>
2023-09-13 22:44:15 +1200

    apply code standards to sgqlc.py

--- app/sgqlc.py
+++ app/sgqlc.py
@@ -1,3 +1,7 @@
+"""
+Functionality for generating query code and fetching data
+- fetch_queried_result
+"""
 import re
 
 from fastapi import HTTPException, status
@@ -7,24 +11,34 @@ from app.sgqlc_schema import Query
 
 
 class SimpleGraphQLClient:
+    """
+    submission -> Gen3 submission is required
+    """
+
     def __init__(self, submission):
-        self.SUBMISSION = submission
+        self._submission = submission
 
-    def handle_node_suffix(self, node, snake_case_query):
-        gen3_node = ""
+    def _handle_suffix(self, node, snake_case):
+        """
+        Handler for removing node suffix
+        """
+        node_type = ""
         if "experiment" in node:
-            gen3_node = "experiment"
+            node_type = "experiment"
         elif "dataset_description" in node:
-            gen3_node = "dataset_description"
+            node_type = "dataset_description"
         elif "manifest" in node:
-            gen3_node = "manifest"
+            node_type = "manifest"
         elif "case" in node:
-            gen3_node = "case"
-        valid_query = re.sub(node, gen3_node, snake_case_query)
-        valid_node = re.sub(node, gen3_node, node)
-        return valid_query, valid_node
+            node_type = "case"
+        updated_query = re.sub(node, node_type, snake_case)
+        updated_node = re.sub(node, node_type, node)
+        return updated_query, updated_node
 
-    def handle_classification(self, item, snake_case_query):
+    def handle_classification(self, item, snake_case):
+        """
+        Handler for processing manifest classification
+        """
         access_scope = re.sub("'", '"', f"{item.access}")
         data = {
             # Choose the number of data to display, 0 here means display everything
@@ -48,48 +62,60 @@ class SimpleGraphQLClient:
             "manifests6": ["dicomImages", "file_type", '[".dcm"]'],
         }
 
-        for key in data:
-            snake_case_query = re.sub(
+        for key, value in data.items():
+            snake_case = re.sub(
                 key,
-                f'{data[key][0]}: manifests(first:0, offset:0, {data[key][1]}: {data[key][2]}, project_id: {access_scope}, order_by_asc:"submitter_id")',
-                snake_case_query,
+                f"{value[0]}: manifests(first:0,"
+                + "offset:0,"
+                + f"{value[1]}: {value[2]},"
+                + f"project_id: {access_scope},"
+                + 'order_by_asc:"submitter_id")',
+                snake_case,
             )
-        return snake_case_query
+        return snake_case
 
-    def handle_null_argument(self, snake_case_query):
-        if "null" in snake_case_query:
-            snake_case_query = re.sub("[,]? [_a-z]+: null", "", snake_case_query)
-        return snake_case_query
+    def _handle_null_argument(self, snake_case):
+        """
+        Handler for removing null from snake case
+        """
+        if "null" in snake_case:
+            snake_case = re.sub("[,]? [_a-z]+: null", "", snake_case)
+        return snake_case
 
-    def handle_snake_case(self, query):
-        snake_case_query = re.sub(
+    def _handle_snake_case(self, query_code):
+        """
+        Handler for converting query code from camel case to snake case
+        """
+        snake_case = re.sub(
             "_[A-Z]",
             lambda x: x.group(0).lower(),
-            re.sub("([a-z])([A-Z])", r"\1_\2", str(query)),
+            re.sub("([a-z])([A-Z])", r"\1_\2", str(query_code)),
         )
-        return snake_case_query
+        return snake_case
 
-    def convert_query(self, item, query):
+    def _handle_query_code_format(self, item, query_code):
+        """
+        Handler for converting query code
+        """
         # Convert camel case to snake case
-        snake_case_query = self.handle_snake_case(query)
-        # Remove all null filter arguments, this can simplify the generate_query function
-        snake_case_query = self.handle_null_argument(snake_case_query)
+        snake_case = self._handle_snake_case(query_code)
+        # Remove all null filter arguments, simplify the _handle_graphql_query_code function
+        snake_case = self._handle_null_argument(snake_case)
         # Either pagination or experiment node query
         if "experiment" in item.node and "count" not in item.node:
-            snake_case_query = self.handle_classification(item, snake_case_query)
-        snake_case_query, item.node = self.handle_node_suffix(
-            item.node, snake_case_query
-        )
-        return "{" + snake_case_query + "}"
+            snake_case = self.handle_classification(item, snake_case)
+        snake_case, item.node = self._handle_suffix(item.node, snake_case)
+        return "{" + snake_case + "}"
 
     # generated query will fetch all the fields that Gen3 metadata has
-    def generate_query(self, item):
+    def _handle_graphql_query_code(self, item):
+        """
+        Handler for creating graphql query code
+        """
         query = Operation(Query)
-        # FILTER
-        # if the node name contains "_filter",
-        # the query generator will be used for /filter/ and /graphql/pagination API
+
         if item.node == "experiment_filter":
-            return self.convert_query(
+            graphql_query_code = self._handle_query_code_format(
                 item,
                 query.experimentFilter(
                     first=0,
@@ -99,7 +125,7 @@ class SimpleGraphQLClient:
                 ),
             )
         elif item.node == "dataset_description_filter":
-            return self.convert_query(
+            graphql_query_code = self._handle_query_code_format(
                 item,
                 query.datasetDescriptionFilter(
                     first=0,
@@ -109,7 +135,7 @@ class SimpleGraphQLClient:
                 ),
             )
         elif item.node == "manifest_filter":
-            return self.convert_query(
+            graphql_query_code = self._handle_query_code_format(
                 item,
                 query.manifestFilter(
                     first=0,
@@ -119,7 +145,7 @@ class SimpleGraphQLClient:
                 ),
             )
         elif item.node == "case_filter":
-            return self.convert_query(
+            graphql_query_code = self._handle_query_code_format(
                 item,
                 query.caseFilter(
                     first=0,
@@ -130,16 +156,14 @@ class SimpleGraphQLClient:
                     project_id=item.access,
                 ),
             )
-        # QUERY
-        # if the node name contains "_query",
-        # the query generator will only be used for /graphql/query API
+
         elif item.node == "experiment_query":
-            if type(item.search) == str and item.search != "":
+            if isinstance(item.search, str) and item.search != "":
                 raise HTTPException(
                     status_code=status.HTTP_405_METHOD_NOT_ALLOWED,
                     detail="Search function does not support while querying in experiment node",
                 )
-            return self.convert_query(
+            graphql_query_code = self._handle_query_code_format(
                 item,
                 query.experimentQuery(
                     first=0,
@@ -149,7 +173,7 @@ class SimpleGraphQLClient:
                 ),
             )
         elif item.node == "dataset_description_query":
-            return self.convert_query(
+            graphql_query_code = self._handle_query_code_format(
                 item,
                 query.datasetDescriptionQuery(
                     first=0,
@@ -159,7 +183,7 @@ class SimpleGraphQLClient:
                 ),
             )
         elif item.node == "manifest_query":
-            return self.convert_query(
+            graphql_query_code = self._handle_query_code_format(
                 item,
                 query.manifestQuery(
                     first=0,
@@ -169,7 +193,7 @@ class SimpleGraphQLClient:
                 ),
             )
         elif item.node == "case_query":
-            return self.convert_query(
+            graphql_query_code = self._handle_query_code_format(
                 item,
                 query.caseQuery(
                     first=0,
@@ -182,7 +206,7 @@ class SimpleGraphQLClient:
         # if the node name contains "_pagination",
         # the query generator will only be used for /graphql/pagination API
         elif item.node == "experiment_pagination":
-            return self.convert_query(
+            graphql_query_code = self._handle_query_code_format(
                 item,
                 query.experimentPagination(
                     first=item.limit,
@@ -194,7 +218,7 @@ class SimpleGraphQLClient:
                 ),
             )
         elif item.node == "experiment_pagination_count":
-            return self.convert_query(
+            graphql_query_code = self._handle_query_code_format(
                 item,
                 query.experimentPaginationCount(
                     first=0,
@@ -205,7 +229,7 @@ class SimpleGraphQLClient:
             )
         # SUPPORT FOR PAGINATION ORDER
         elif item.node == "pagination_order_by_dataset_description":
-            return self.convert_query(
+            graphql_query_code = self._handle_query_code_format(
                 item,
                 query.paginationOrderByDatasetDescription(
                     first=item.limit,
@@ -221,19 +245,25 @@ class SimpleGraphQLClient:
                 status_code=status.HTTP_404_NOT_FOUND,
                 detail="GraphQL query cannot be generated by sgqlc",
             )
+        return graphql_query_code
 
-    def get_queried_result(self, item, key=None, queue=None):
-        if item.node == None:
+    def fetch_queried_result(self, item, key=None, queue=None):
+        """
+        Handler for fetching gen3 data with graphql query code
+        """
+        if item.node is None:
             raise HTTPException(
                 status_code=status.HTTP_400_BAD_REQUEST,
                 detail="Missing one or more fields in the request body",
             )
 
-        query = self.generate_query(item)
+        query = self._handle_graphql_query_code(item)
         try:
-            result = self.SUBMISSION.query(query)["data"][item.node]
-            if key != None and queue != None:
+            result = self._submission.query(query)["data"][item.node]
+            if key is not None and queue is not None:
                 queue.put({key: result})
             return result
-        except Exception as e:
-            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=str(e))
+        except Exception as error:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND, detail=str(error)
+            ) from error
