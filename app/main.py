69786bece587c395113f4573bafb59315f97fa86
ddjnw1yu <david.jianwei.yu@outlook.com>
2023-10-03 15:48:58 +1300

    format

--- app/main.py
+++ app/main.py
@@ -361,10 +361,7 @@ async def get_gen3_graphql_query(
             status_code=status.HTTP_400_BAD_REQUEST,
             detail="Invalid query node is used",
         )
-    if (
-        item.node == "experiment_query"
-        and item.search != ""
-    ):
+    if item.node == "experiment_query" and item.search != "":
         raise HTTPException(
             status_code=status.HTTP_405_METHOD_NOT_ALLOWED,
             detail="Search does not provide in current node",
